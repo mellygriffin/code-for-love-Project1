20014defb1955583d22afab63db6b7e44bffa9cd
MellGriffin <missmelissagriffin@gmail.com>
2024-07-11 16:47:21 -0500

    Added classes to html, some javascript

--- js/app.js
+++ js/app.js
@@ -1,13 +1,34 @@
 /*-------------- Constants -------------*/
-
+const winningScore = 10;
+const greatPoints = 2;
+const goodPoints = 1;
+const badPoints = 0;
 
 /*---------- Variables (state) ---------*/
+let playerChoice = null;
+let displayScene = null;
+let progressBar = 0;
 
 
 /*----- Cached Element References  -----*/
-
+let playerScore = 0;
+const npcDialogue = document.querySelector('.npc-text');
 
 /*-------------- Functions -------------*/
+const init = () => {
+    displayScene();
+    render();
+}
 
+const render = () => {
+    //
+}
+
+class Scenes {
+    //
+}
 
 /*----------- Event Listeners ----------*/
+const handleClick = () => {
+
+}
