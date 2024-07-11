c869bd8231e762cb9e8d71a3b12abb6d90b3ddb1
MellGriffin <missmelissagriffin@gmail.com>
2024-07-11 18:49:03 -0500

    Created arrays for questions

--- js/app.js
+++ js/app.js
@@ -8,6 +8,7 @@ const badPoints = 0;
 let playerChoice = null;
 let displayScene = null;
 let progressBar = 0;
+let whichQuestion = 0;
 
 
 /*----- Cached Element References  -----*/
@@ -24,6 +25,16 @@ const render = () => {
     //
 }
 
+//two arrays: consts - Hobbies Questions, About You Questions
+//five objects in each array, for each questions
+//properties: question, key: What would youb like to talk
+//options: Choices array, objects
+
+//First question chooses what array you're using!
+
+//access array, at this array at this question, at this answer
+//.options
+
 class Scenes {
     //
 }
@@ -32,3 +43,92 @@ class Scenes {
 const handleClick = () => {
 
 }
+
+/*-------------- Array -------------*/
+
+const introQuestion = [
+    {questionIntro: 'What would you like to talk about?',
+        options: [
+            {answer: 'About You', choice: ''},
+            {answer: 'Hobbies and Skills', choice: ''},
+            {answer: 'Nothing, you can\'t talk you\'re a computer!', choice: ''}
+        ]
+    }
+]
+
+const aboutYou = [
+    {question1: 'What operating system do you like?',
+        options: [
+            {answer: 'Windows', choice: 'great'},
+            {answer: 'Mac', choice: 'good'},
+            {answer: 'I don\'t really like computers...'}
+
+        ]
+    },
+    {question2: 'What do you like about going to a cafe?',
+        options: [
+            {answer: 'Peace and quiet', choice: 'good'},
+            {answer: 'Free WiFi', choice: 'great'},
+            {answer: 'A nice cup of coffee', choice: 'bad'}
+        ]
+    },
+    {question3: 'What kind of video games do you like?',
+        options: [
+            {answer: 'Cozy Farming Sims', choice: 'good'},
+            {answer: 'Rhythm Games', choice: 'bad'},
+            {answer: 'Roleplaying Adventures', choice: 'great'}
+        ]
+    },
+    {question4: 'What\'s your favorite sport?',
+        options: [
+            {answer: 'Baseball', choice: 'bad'},
+            {answer: 'Pong', choice: 'great'},
+            {answer: 'Tennis', choice: 'good'}
+        ]
+    },
+    {question5: 'Do you like to travel?',
+        options: [
+            {answer: 'Yes', choice: 'good'},
+            {answer: 'No', choice: 'bad'},
+            {answer: 'As long as I have an extension cord...', choice: 'great'}
+        ]
+    }
+]
+
+const hobbiesInterests = [
+    {question1: 'What do you do on a rainy day?',
+        options: [
+            {answer: 'Stay inside and read.', choice: 'good'},
+            {answer: 'Go outside and dance in the rain!', choice: 'bad'},
+            {answer: 'Cozy up and game all day.', choice: 'great'}
+        ]
+    },
+    {question2: 'Do you have a hobby?',
+        options: [
+            {answer: 'Making Arts and Crafts', choice: 'good'},
+            {answer: 'Sports and Exercise', choice: 'bad'},
+            {answer: 'Playing Video Games', choice: 'great'}
+        ]
+    },
+    {question3: 'What\'s your favorite color?',
+        options: [
+            {answer: 'Blue', choice: 'good'},
+            {answer: 'Red', choice: 'bad'},
+            {answer: '#808080', choice: 'great'}
+        ]
+    },
+    {question4: 'What\'s your dream job?',
+        options: [
+            {answer: 'Software Engineer', choice: 'great'},
+            {answer: 'Artist', choice: 'good'},
+            {answer: 'Doctor', choice: 'bad'}
+        ]
+    },
+    {question5: 'Do you like to travel?',
+        options: [
+            {answer: 'Yes', choice: 'good'},
+            {answer: 'No', choice: 'bad'},
+            {answer: 'As long as I have an extension cord...', choice: 'great'}
+        ]
+    }
+]
