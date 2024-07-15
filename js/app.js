9aa54f0d2d961cb68c307b277fddb3be5830b758
MellGriffin <missmelissagriffin@gmail.com>
2024-07-15 16:40:51 -0500

    Got points to count

--- js/app.js
+++ js/app.js
@@ -16,31 +16,31 @@ const introQuestion = [
 const aboutYou = [
     {question: 'What operating system do you like?',
         options: [
-            {answer: 'Windows', choice: 'great'},
-            {answer: 'Mac', choice: 'good'},
-            {answer: 'I don\'t really like computers...'}
+            {answer: 'Windows', choice: 2},
+            {answer: 'Mac', choice: 1},
+            {answer: 'I don\'t really like computers...', choice: 0}
 
         ]
     },
     {question: 'What do you like about going to a cafe?',
         options: [
-            {answer: 'Peace and quiet', choice: 'good'},
-            {answer: 'Free WiFi', choice: 'great'},
-            {answer: 'A nice cup of coffee', choice: 'bad'}
+            {answer: 'Peace and quiet', choice: 1},
+            {answer: 'Free WiFi', choice: 2},
+            {answer: 'A nice cup of coffee', choice: 0}
         ]
     },
     {question: 'What kind of video games do you like?',
         options: [
-            {answer: 'Cozy Farming Sims', choice: 'good'},
-            {answer: 'Rhythm Games', choice: 'bad'},
-            {answer: 'Roleplaying Adventures', choice: 'great'}
+            {answer: 'Cozy Farming Sims', choice: 1},
+            {answer: 'Rhythm Games', choice: 0},
+            {answer: 'Roleplaying Adventures', choice: 2}
         ]
     },
     {question: 'What\'s your favorite sport?',
         options: [
-            {answer: 'Baseball', choice: 'bad'},
-            {answer: 'Pong', choice: 'great'},
-            {answer: 'Tennis', choice: 'good'}
+            {answer: 'Baseball', choice: 0},
+            {answer: 'Pong', choice: 2},
+            {answer: 'Tennis', choice: 1}
         ]
     },
     {question: 'Do you like to travel?',
@@ -55,46 +55,43 @@ const aboutYou = [
 const hobbiesInterests = [
     {question: 'What do you do on a rainy day?',
         options: [
-            {answer: 'Stay inside and read.', choice: 'good'},
-            {answer: 'Go outside and dance in the rain!', choice: 'bad'},
-            {answer: 'Cozy up and game all day.', choice: 'great'}
+            {answer: 'Stay inside and read.', choice: 1},
+            {answer: 'Go outside and dance in the rain!', choice: 0},
+            {answer: 'Cozy up and game all day.', choice: 2}
         ]
     },
     {question: 'Do you have a hobby?',
         options: [
-            {answer: 'Making Arts and Crafts', choice: 'good'},
-            {answer: 'Sports and Exercise', choice: 'bad'},
-            {answer: 'Playing Video Games', choice: 'great'}
+            {answer: 'Making Arts and Crafts', choice: 1},
+            {answer: 'Sports and Exercise', choice: 0},
+            {answer: 'Playing Video Games', choice: 2}
         ]
     },
     {question: 'What\'s your favorite color?',
         options: [
-            {answer: 'Blue', choice: 'good'},
-            {answer: 'Red', choice: 'bad'},
-            {answer: '#808080', choice: 'great'}
+            {answer: 'Blue', choice: 1},
+            {answer: 'Red', choice: 0},
+            {answer: '#808080', choice: 2}
         ]
     },
     {question: 'What\'s your dream job?',
         options: [
-            {answer: 'Software Engineer', choice: 'great'},
-            {answer: 'Artist', choice: 'good'},
-            {answer: 'Doctor', choice: 'bad'}
+            {answer: 'Software Engineer', choice: 3},
+            {answer: 'Artist', choice: 1},
+            {answer: 'Doctor', choice: 0}
         ]
     },
     {question: 'Do you like to travel?',
         options: [
-            {answer: 'Yes', choice: 'good'},
-            {answer: 'No', choice: 'bad'},
-            {answer: 'As long as I have an extension cord...', choice: 'great'}
+            {answer: 'Yes', choice: 1},
+            {answer: 'No', choice: 0},
+            {answer: 'As long as I have an extension cord...', choice: 2}
         ]
     }
 ]
 
 /*-------------- Constants -------------*/
 const winningScore = 10;
-const greatPoints = 2;
-const goodPoints = 1;
-const badPoints = 0;
 const choices = [];
     
     
@@ -103,6 +100,7 @@ let playerChoice = null;
 let progressBar = 0;
 let whichQuestion = 0;
 let category = null;
+let playerPoints = 0;
 
 
 /*----- Cached Element References  -----*/
@@ -127,13 +125,34 @@ const questionDisplay =  (choice) => {
     document.getElementById('third').innerText = choice[whichQuestion].options[2].answer
 }
 
-const categorySelect = (choice) => {
-    //
+// const categorySelect = (choice) => {
+//     //
+// }
+
+const pointValue = (choice) => {
+    console.log(choice)
+    if (choice === 'first') {
+        playerPoints = playerPoints + category[whichQuestion].options[0].choice;
+    } else if (choice === 'second') {
+        playerPoints = playerPoints + category[whichQuestion].options[1].choice;
+    } else if (choice === 'third') {
+        playerPoints = playerPoints + category[whichQuestion].options[2].choice;
+    }
+}
+
+
+const displayPoints = () => {
+    document.getElementById('pointsCounter').innerText = playerPoints
 }
 
 const init = () => {
     introduction();
+    playerPoints = 0;
     // play();
+}
+
+const render = () => {
+    displayPoints()
 }
 
 const play = function(event){
@@ -146,14 +165,27 @@ const play = function(event){
     } else if (event.target.id === 'third' && !category) {
         console.log('End game')
     } else {
+        pointValue(event.target.id)
         whichQuestion = whichQuestion + 1
         questionDisplay(category)
     }
+    render()
 }
 
+
+
+// const questionCount = () => {
+//     document.getElementById('numberLeft').innerText = whichQuestion[i++] + "/5";
+// }
+
+
 const endGame = () => {
-    if (question === 4) {
-        
+    if (whichQuestion === 4 && playerPoints === 10) {
+        console.log('You win!')
+    } else if (whichQuestion === 4 && playerPoints < 10) {
+        console.log('You lose!')
+    } else {
+        console.log('You lose!')
     }
 }
 
